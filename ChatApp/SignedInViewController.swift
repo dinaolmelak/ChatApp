10546ae4df6d46736d6681d9a2b878c376364497
dinaolmelak <dinaolmelak@gmail.com>
2020-01-16 11:43:39 -0600

    Initial Commit

---
+++ ChatApp/SignedInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignedInViewController.swift
+//  ChatApp
+//
+//  Created by Center for Innovation on 11/11/19.
+//  Copyright © 2019 Center for Innovation. All rights reserved.
+//
+
+import UIKit
+
+class SignedInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Center for Innovation on 11/11/19.
+//  Copyright © 2019 Center for Innovation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usernameLabel: UITextField!
+    @IBOutlet weak var passwordLabel: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func didTapLogIn(_ sender: Any) {
+        print("Log in")
+        
+    }
+    @IBAction func didTapSignUp(_ sender: Any) {
+        
+        print("Sign UP")
+    }
+    @IBAction func didDragCart(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        let kartView = sender.view!
+        kartView.center = location
+        
+        
+    }
+    @IBAction func didTap(_ sender: Any) {
+        print("didTap")
+        view.endEditing(true)
+        
+    }
+    @IBAction func didPinchKart(_ sender: UIPinchGestureRecognizer) {
+        let scale = sender.scale
+        print(scale)
+        let kartView = sender.view
+        kartView?.transform = CGAffineTransform(scaleX: scale, y: scale)
+        
+        
+    }
+    
+
+}
+
